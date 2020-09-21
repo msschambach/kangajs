bddb1246dfa547d5bc9672d3eb8e25a7ce6d3ff9
Schambach <msschambach@users.noreply.github.com>
2020-09-21 09:04:52 +0300

    Rebuilt project

--- dist/BrowserStore.d.ts
+++ dist/BrowserStore.d.ts
@@ -1,18 +1,18 @@
 import Record from './Record';
 declare type StorageMode = 'localStorage' | 'sessionStorage';
 declare class BrowserStore {
-  mode: StorageMode;
-  private readonly $storage;
-  constructor(shouldUseLocalStorage?: boolean);
-  get length(): number;
-  parse<R>(value: string): R | undefined;
-  find<R>(key: string): Record<R> | null;
-  findAll(): Record<unknown>[];
-  findAt<R>(index: number, returnAsRecord?: boolean): Record<R> | string | null;
-  indexOf(key: string): number | undefined;
-  set<R>(key: string, value: R | string): BrowserStore;
-  delete(key: string): BrowserStore;
-  deleteAll(): void;
-  log(key: string): BrowserStore;
+    mode: StorageMode;
+    private readonly $storage;
+    constructor(shouldUseLocalStorage?: boolean);
+    get length(): number;
+    parse<R>(value: string): R | undefined;
+    find<R>(key: string): Record<R> | null;
+    findAll(): Record<unknown>[];
+    findAt<R>(index: number, returnAsRecord?: boolean): Record<R> | string | null;
+    indexOf(key: string): number | undefined;
+    set<R>(key: string, value: R | string): BrowserStore;
+    delete(key: string): BrowserStore;
+    deleteAll(): void;
+    log(key: string): BrowserStore;
 }
 export default BrowserStore;
